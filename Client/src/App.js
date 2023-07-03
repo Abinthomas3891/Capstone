933703c1fcc002d4e300fb9bc28b53e36541706e
Nithinjs615 <nithinjs615@gmail.com>
2023-07-03 17:01:32 -0400

    update db

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,14 +2,29 @@ import React from 'react';
 import Contactus from './contact';
 import Home from './home';
 import About from './aboutus';
+import DisplayUser from "./DisplayData"
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, ApolloLink, InMemoryCache,createHttpLink } from "@apollo/client";
 import { Routes, Route } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4500",
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([httpLink]),
+});
+
 const App = () => {
   return (
-    <Routes>
+    <ApolloProvider client={client}>
+      <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contactus />} />
-    </Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contactus />} />
+      </Routes>
+    </ApolloProvider>
   );
 };
 
